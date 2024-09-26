5d873399c512b981368fb347002842ac0d9bb820
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2024-09-26 20:11:12 +0530

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "web development" , "UI development" , "UX development"],
+    strings: ["frontend developer", "backend developer", "web developer", "web developer" , "UI developer" , "UX developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
